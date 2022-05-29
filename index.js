5271cbe7f622cf0426ef37fe98c5c0b72ea60b5e
Le Hong vo <volhfx07132@funix.edu.vn>
2022-05-29 10:55:33 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-//index.js
-
-const Web3 = require("web3");
-
-const provider =
-  "https://speedy-nodes-nyc.moralis.io/2cac97baa7c919b144ee567f/eth/rinkeby/archive"
-
-const Web3Client = new Web3(new Web3.providers.HttpProvider(provider));
-
-// The minimum ABI required to get the ERC20 Token balance
-const minABI = [
+var Web3 = require("web3");
+// var web3 = new Web3("https://speedy-nodes-nyc.moralis.io/2cac97baa7c919b144ee567f/eth/rinkeby");
+var web3 = new Web3("HTTP://127.0.0.1:7545");
+var sender = "0x3Cb06A7709a2511e48a091979b9b68B27999AfaB";
+var receiver = "0xCdA189005BFBe90bc2D518f69FfB937EcdFfB3f3";
+var contratAddress = "0xaE4F746E9b138807c28F1ce96d3A7cc0D118303A";
+var abi = [
 	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
@@ -40,6 +36,74 @@ const minABI = [
 		"type": "event"
 	},
 	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
 		"inputs": [
 			{
 				"internalType": "address",
@@ -67,6 +131,38 @@ const minABI = [
 		"inputs": [
 			{
 				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
 				"name": "spender",
 				"type": "address"
 			},
@@ -85,6 +181,19 @@ const minABI = [
 			}
 		],
 		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getCurrentTime",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
 		"type": "function"
 	},
 	{
@@ -112,29 +221,62 @@ const minABI = [
 		"type": "function"
 	},
 	{
-		"anonymous": false,
-		"inputs": [
+		"inputs": [],
+		"name": "name",
+		"outputs": [
 			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "previousOwner",
-				"type": "address"
-			},
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
 			{
-				"indexed": true,
 				"internalType": "address",
-				"name": "newOwner",
+				"name": "",
 				"type": "address"
 			}
 		],
-		"name": "OwnershipTransferred",
-		"type": "event"
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"inputs": [],
 		"name": "renounceOwnership",
 		"outputs": [],
 		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
 		"type": "function"
 	},
 	{
@@ -160,31 +302,6 @@ const minABI = [
 		],
 		"stateMutability": "nonpayable",
 		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "Transfer",
-		"type": "event"
 	},
 	{
 		"inputs": [
@@ -227,140 +344,32 @@ const minABI = [
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "spender",
-				"type": "address"
-			}
-		],
-		"name": "allowance",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "account",
-				"type": "address"
-			}
-		],
-		"name": "balanceOf",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "decimals",
-		"outputs": [
-			{
-				"internalType": "uint8",
-				"name": "",
-				"type": "uint8"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getCurrentTime",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "name",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "symbol",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "totalSupply",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
 	}
 ]
-const tokenAddress = "0xd213159Cb5a5a9242BD859B61Fa788840a7c0A65";
-const walletAddress = "0x3Cb06A7709a2511e48a091979b9b68B27999AfaB";
+//contract 
+// var contract = new web3.eth.Contract(abi, contratAddress);
+// var nameToken;
 
-const contract = new Web3Client.eth.Contract(minABI, tokenAddress);
+// contract.methods.name().call((err, result) => {
+// 	console.log(result);
+// })
 
-async function getBalance() {
-  const result = await contract.methods.balanceOf(walletAddress).call();
-  const balanceBefor = Web3Client.utils.fromWei(result);
-  console.log("Balance of befor transfer: "+balanceBefor);
-  let withdrawProfits = await Web3Client.connect(owner).transfer("0xCdA189005BFBe90bc2D518f69FfB937EcdFfB3f3", 10000000, {from: tokenAddress});
-  await withdrawProfits.wait();
-}
+// contract.methods.symbol().call((err, result) => {
+// 	console.log(result);
+// })
 
-getBalance();
+// contract.methods.balanceOf("0x3Cb06A7709a2511e48a091979b9b68B27999AfaB").call((err, result) => {
+// 	console.log(web3.utils.fromWei(result));
+// })
+var sender = "0x3Cb06A7709a2511e48a091979b9b68B27999AfaB";
+var receiver = "0xCdA189005BFBe90bc2D518f69FfB937EcdFfB3f3";
+
+// web3.eth.getBalance(sender, (err, balance) => {
+// 	console.log(web3.utils.fromWei(balance));
+// })
+// var txHash = web3.eth.sendTransaction({from: sender, to: receiver, value: web3.utils.toWei("1","ether")});
+
+// console.log(txHash);
+web3.eth.personal
+
+
---
+++ main.js
@@ -0,0 +1,34 @@
+var Tx     = require('ethereumjs-tx') 
+const Web3 = require('web3')
+const web3 = new Web3("https://rinkeby.infura.io/v3/5bace1a8bb64466db9ab0b35b5273f81" ||'https://speedy-nodes-nyc.moralis.io/2cac97baa7c919b144ee567f/eth/rinkeby')
+
+const account1 = '0x3Cb06A7709a2511e48a091979b9b68B27999AfaB'
+const account2 = '0xCdA189005BFBe90bc2D518f69FfB937EcdFfB3f3'
+
+var privateKey1 = Buffer.from('b202eca8eb24ce8fed61a6f907c16defafeb81af037333b4331f2607ed5533b0', 'hex');
+
+web3.eth.getTransactionCount(account1,(err,txCount)=>{
+    //building a transaction
+    const txObject = {
+		nonce:    web3.utils.toHex(txCount),
+		to:       account2,
+		value:    web3.utils.toHex(web3.utils.toWei('1', 'ether')),
+		gasLimit: web3.utils.toHex(21000),
+		gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
+  }
+
+  // Sign the transaction
+  const tx = new Tx(txObject)
+  tx.sign(privateKey1)
+
+  const serializedTx = tx.serialize()
+  const raw = '0x' + serializedTx.toString('hex')
+
+  // Broadcast the transaction
+  web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+    console.log('txHash:', txHash)
+  })
+})
+
+
+
